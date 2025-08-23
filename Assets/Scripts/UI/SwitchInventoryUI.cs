c1b8ba6218a0526999176df1d0c749d0c983b241
DylanPatrascu <dylanpatrascu1@gmail.com>
2025-08-23 16:36:59 -0400

    fixed prefabs, starting deck tab

--- Assets/Scripts/UI/SwitchInventoryUI.cs
+++ Assets/Scripts/UI/SwitchInventoryUI.cs
@@ -4,15 +4,25 @@ public class SwitchInventoryUI : MonoBehaviour
 {
     public GameObject cardCanvas;
     public GameObject packCanvas;
+    public GameObject deckCanvas;
 
     public void EnablePackCanvas()
     {
         packCanvas.SetActive(true);
         cardCanvas.SetActive(false);
+        deckCanvas.SetActive(false);
     }
     public void EnableCardCanvas()
     {
         cardCanvas.SetActive(true);
+        packCanvas.SetActive(false);
+        deckCanvas.SetActive(false);
+    }
+
+    public void EnableDeckCanvas()
+    {
+        deckCanvas.SetActive(true);
+        cardCanvas.SetActive(false);
         packCanvas.SetActive(false);
     }
 }
